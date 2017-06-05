0556b150ffd98bb1678f31096b63d50795669e95
Omar Ansari <omarahmedansari@gmail.com>
2017-06-04 22:10:22 -0400

    Initial Commit

---
+++ Ansario.Web.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Ansario.Web.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Ansario.Web.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ansario.Web.API.WebApiApplication" Language="C#" %>
